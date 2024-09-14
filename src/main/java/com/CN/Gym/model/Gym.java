586aa99f7297a9884e0c34b63ea5e84449d6426d
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-14 21:38:24 +0530

    adding Gym, User, Workout Entity

---
+++ src/main/java/com/CN/Gym/model/Gym.java
@@ -0,0 +1,40 @@
+package com.CN.Gym.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/*
+    This is the entity class, complete this class by doing the following:
+    a. Add the required annotations for making this class an entity.
+    b. Add the required lombok annotations for getter, setter and constructors
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Gym {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String address;
+    private Long contactNo;
+    private String membershipPlans;
+    private String facilities;
+
+    @OneToMany(mappedBy = "gym", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<User> users = new ArrayList<>();
+
+}
