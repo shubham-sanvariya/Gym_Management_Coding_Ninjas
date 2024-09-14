678629dd78055e78d6361bac5f38ba3bc8d1ce22
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-14 21:58:27 +0530

    adding Gym Service

---
+++ src/main/java/com/CN/Gym/service/GymService.java
@@ -0,0 +1,50 @@
+package com.CN.Gym.service;
+
+
+import com.CN.Gym.model.Gym;
+import com.CN.Gym.repository.GymRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GymService {
+
+    /*
+        This is the service class for Gym, you need to complete the class by doing the following:
+        a. Use appropriate annotations.
+        b. Complete the methods given below.
+        c. Autowire the necessary dependencies.
+     */
+
+
+    // public List<Gym> getAllGyms() {   
+
+    // }
+
+    //  public Gym getGymById(Long id) {
+
+    // }
+
+    // public void deleteGymById(Long id) {        
+
+    // }
+
+    // public void updateGym(GymDto gymDto, Long id) {
+
+    // }
+
+    // public void createGym(GymDto gymDto) {
+
+    // }
+
+    // public void addMember(Long userId, Long gymId) {
+
+    // }
+
+    // public void deleteMember(Long userId, Long gymId) {
+
+    // }
+}
