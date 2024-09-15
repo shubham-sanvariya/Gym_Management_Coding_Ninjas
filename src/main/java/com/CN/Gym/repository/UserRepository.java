81d0d3ecf150d0e73bd31018f24bbf96496d3e69
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-15 14:50:57 +0530

    adding method findByEmail in user repo

--- src/main/java/com/CN/Gym/repository/UserRepository.java
+++ src/main/java/com/CN/Gym/repository/UserRepository.java
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByEmail(String email);
 }
