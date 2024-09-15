29357eeaea37c52e913bf8a516d6cb887472d0f0
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-15 13:47:01 +0530

    adding GymSecurityConfig

---
+++ src/main/java/com/CN/Gym/config/GymSecurityConfig.java
@@ -0,0 +1,13 @@
+package com.CN.Gym.config;
+
+
+public class GymSecurityConfig {
+
+    /*
+        1. Add relevant annotations to make this class a configuration class and enable method leve security.
+        2. Create authenticationManager, passwordEncoder bean.
+        3. Create securityFilterChain bean with httpBasic security and use antMatcher to make "/user/register"
+           an open API.
+     */
+
+}
