5bb33a18f04cd818b0911089b23de14271e2ceea
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-15 13:53:51 +0530

    adding passwordEncoder bean method in GymSecurityConfig

--- src/main/java/com/CN/Gym/config/GymSecurityConfig.java
+++ src/main/java/com/CN/Gym/config/GymSecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -37,6 +39,11 @@ public class GymSecurityConfig {
      @Bean
      public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception{
         return builder.getAuthenticationManager();
+     }
+
+     @Bean
+     public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
      }
 
 }
