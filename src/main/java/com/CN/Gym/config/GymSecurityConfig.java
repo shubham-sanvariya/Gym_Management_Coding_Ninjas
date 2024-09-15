f28ff8ff575f92432ee1e717c6bf689e2663f970
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-15 13:53:11 +0530

    adding authenticationManager bean method in GymSecurityConfig

--- src/main/java/com/CN/Gym/config/GymSecurityConfig.java
+++ src/main/java/com/CN/Gym/config/GymSecurityConfig.java
@@ -2,6 +2,8 @@ package com.CN.Gym.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -30,6 +32,11 @@ public class GymSecurityConfig {
             .httpBasic();
         
         return http.build();
+     }
+
+     @Bean
+     public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception{
+        return builder.getAuthenticationManager();
      }
 
 }
