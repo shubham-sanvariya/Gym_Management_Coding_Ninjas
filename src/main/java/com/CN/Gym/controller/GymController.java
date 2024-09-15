c8e624ec870e04b32520c8222d3eb8a48e3b2d1e
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-15 13:15:52 +0530

    adding addMember mapping in Gym Controller

--- src/main/java/com/CN/Gym/controller/GymController.java
+++ src/main/java/com/CN/Gym/controller/GymController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -65,8 +66,9 @@ public class GymController {
 
     // 6. POST "/gym/addMember" (@RequestParam Long userId, @RequestParam Long gymId): This API allows the admin to add users to a particular gym by passing userId
     //                                                                                 and gymId as requestParam. It returns a CREATED HTTP status.
-    public void addMember() {
-
+    @PostMapping("/addMember")
+    public void addMember(@RequestParam Long userId, @RequestParam Long gymId) {
+        gymService.addMember(userId,gymId);
     }
 
 
