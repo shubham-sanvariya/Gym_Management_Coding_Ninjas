3b8ee0b7db0aab8bf38eb1c4996bc7fba9f87238
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-16 17:13:16 +0530

    adding JwtAuthenticationFilter component

---
+++ src/main/java/com/CN/Gym/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,24 @@
+package com.CN.Gym.jwt;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+    }
+    
+
+}
