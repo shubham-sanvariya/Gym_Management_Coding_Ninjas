f0de9c7c3c43ad2cd756d4d8c006a12d5f6033ab
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-16 17:35:23 +0530

    adding getClaimsFromToken in Authentication helper

--- src/main/java/com/CN/Gym/jwt/JwtAuthenticationHelper.java
+++ src/main/java/com/CN/Gym/jwt/JwtAuthenticationHelper.java
@@ -2,8 +2,18 @@ package com.CN.Gym.jwt;
 
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
 @Component
 public class JwtAuthenticationHelper {
+
+    private static final String TOKEN_SECRECT_KEY = "ValarMorghulisvalardohaeris";
     
-    
+    public Claims getClaimsFromToken(String token){
+        Claims claims = Jwts.parserBuilder().setSigningKey(TOKEN_SECRECT_KEY.getBytes())
+                            .build().parseClaimsJws(token).getBody();
+                        
+            return claims;
+    }
 }
