fd86432389c80d8f892ce96ec0bb884462da08f8
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-16 17:01:53 +0530

    adding method for authenticating user and password

--- src/main/java/com/CN/Gym/service/AuthService.java
+++ src/main/java/com/CN/Gym/service/AuthService.java
@@ -1,5 +1,9 @@
 package com.CN.Gym.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 import com.CN.Gym.dto.JwtRequest;
@@ -7,6 +11,22 @@ import com.CN.Gym.dto.JwtResponse;
 
 @Service
 public class AuthService {
+
+    @Autowired
+    AuthenticationManager manager;
     
-    
+    public JwtResponse getResponse(JwtRequest jwtRequest){
+        this.doAuthenticate(jwtRequest.getUsername(),jwtRequest.getPassword());
+
+        return null;
+    }
+
+    private void doAuthenticate(String username, String password) {
+        UsernamePasswordAuthenticationToken upat = new UsernamePasswordAuthenticationToken(username,password);
+        try {
+            manager.authenticate(upat);
+        } catch (BadCredentialsException e) {
+            throw new BadCredentialsException("Invalid username or password");
+        }
+    }
 }
