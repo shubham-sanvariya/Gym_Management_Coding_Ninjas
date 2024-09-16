56bcf5a3db0256409b01ebf20716baedb291aed4
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-16 20:51:31 +0530

    adding mapping for auth/login in auth controller

--- src/main/java/com/CN/Gym/controller/AuthController.java
+++ src/main/java/com/CN/Gym/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.CN.Gym.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,10 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.CN.Gym.dto.JwtRequest;
 import com.CN.Gym.dto.JwtResponse;
+import com.CN.Gym.service.AuthService;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
+    @Autowired
+    private AuthService authService;
     
-    
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody JwtRequest request){
+        return new ResponseEntity<JwtResponse>(authService.getResponse(request),HttpStatus.OK);
+    }
 }
