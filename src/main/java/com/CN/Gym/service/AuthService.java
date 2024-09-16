3a4475d28e3626e3d257ca0702f1470c79207383
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2024-09-16 20:47:55 +0530

    adding token in JwtResponse in auth service

--- src/main/java/com/CN/Gym/service/AuthService.java
+++ src/main/java/com/CN/Gym/service/AuthService.java
@@ -4,21 +4,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 import com.CN.Gym.dto.JwtRequest;
 import com.CN.Gym.dto.JwtResponse;
+import com.CN.Gym.jwt.JwtAuthenticationHelper;
 
 @Service
 public class AuthService {
 
     @Autowired
     AuthenticationManager manager;
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Autowired
+    JwtAuthenticationHelper jwtHelper;
     
     public JwtResponse getResponse(JwtRequest jwtRequest){
         this.doAuthenticate(jwtRequest.getUsername(),jwtRequest.getPassword());
 
-        return null;
+        UserDetails userDetails = userDetailsService.loadUserByUsername(jwtRequest.getUsername());
+
+        String token = jwtHelper.generateToken(userDetails);
+
+        JwtResponse response = new JwtResponse().builder().jwtToken(token).build();
+        return response;
     }
 
     private void doAuthenticate(String username, String password) {
